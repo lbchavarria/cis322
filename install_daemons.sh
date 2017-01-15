0548b8228e1b7c85fff3a2318aed1431102581d3
lbchavarria <lbchavarria@gmail.com>
2017-01-15 22:23:51 +0000

    update install_daemons.sh

--- install_daemons.sh
+++ install_daemons.sh
@@ -7,6 +7,6 @@ adduser osnapdev
 curl -O http://www.trieuvan.com/apache//httpd/httpd-2.4.25.tar.gz
 gunzip httpd-2.4.25.tar.gz
 tar -xvf httpd-2.4.25.tar
-./configure --prefix=$HOME/installed
+./configure 
 make
 make install
