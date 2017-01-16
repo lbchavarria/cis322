b5da5c13226b14262ca4f26e2a8f974341e0ddec
lbchavarria <lbchavarria@gmail.com>
2017-01-16 09:38:02 +0000

    finished install_daemons.sh

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 git clone https://github.com/postgres/postgres.git
+cd postgres/
 ./configure
 make
 make install
-adduser osnapdev
+cd ..
 curl -O http://www.trieuvan.com/apache//httpd/httpd-2.4.25.tar.gz
 gunzip httpd-2.4.25.tar.gz
 tar -xvf httpd-2.4.25.tar
+cd httpd-2.4.25/
 ./configure 
 make
 make install
+cd ..
