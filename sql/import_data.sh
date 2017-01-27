0073ed2453d7f28678f957dcb369b3858ad2503e
lbchavarria <lbchavarria@gmail.com>
2017-01-27 22:17:24 +0000

    updated

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 read dbname
+echo $dbname
 read port_num
+echo $port_num
+
+initdb -D $HOME/sql/home
+echo "init"
+pg_ctl -D $HOME/sql/home -o "-p $port_num" -l $HOME/sql/logfile start
+echo "start"
 
 create $dbname
+echo "create"
 psql $dbname -f create_tables.sql
+echo "psql"
 curl https://classes.cs.uoregon.edu/17W/cis322/files/osnap_legacy.tar.gz > $HOME/sql/osnap_legacy.tar.gz
+echo "curl"
 gunzip $HOME/sql/osnap_legacy.tar.gz
+echo "gunzip"
 tar -xvf $HOME/sql/osnap_legacy.tar
+echo "untar"
 psql $dbname -f load_data_tables.sql
+echo "finish"
