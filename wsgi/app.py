d65f195d7efc8ded8db1c9732ec5d9cb6b85d717
lbchavarria <lbchavarria@gmail.com>
2017-02-12 08:32:57 +0000

    upadate index.html and app.py and add login.html and report.html

--- wsgi/app.py
+++ wsgi/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/report')
 def report():
     return render_template('report.html')
