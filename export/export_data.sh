6dcaab8bf7b0ff7cc37ccee80c7c9d44b49cf1bb
lbchavarria <lbchavarria@gmail.com>
2017-03-07 22:40:51 +0000

    update export_data.sh

--- export/export_data.sh
+++ export/export_data.sh
@@ -14,7 +14,10 @@ fi
 
 cd ../sql
 pg_ctl -D $HOME/cis322/sql/data -l logfile start
-psql -v dir=$2
-psql $1 -f ../sql/export_data.sql
+cd ../export/$2
+psql -d $1 -A -F", " -c "SELECT username, password, title AS role FROM users JOIN roles ON users.role_fk=roles.role_id" > users.csv
+psql -d $1 -A -F", " -c "SELECT code AS fcode, name AS common_name FROM facilities" > facilities.csv
+psql -d $1 -A -F", " -c "SELECT asset_tag, description, name AS facility, disposed FROM assets JOIN asset_at ON assets.asset_id=asset_at.asset_fk JOIN facilities ON facilities.facility_id=asset_at.facility_fk" > assets.csv
+psql -d $1 -A -F", " -c "SELECT asset_tag, requester.username AS request_by, request_time AS request_dt, approver.username AS approve_by, approval_time AS approve_dt, source.name AS source, destination.name AS destination, load AS load_dt, unload AS unload_dt FROM trans_request JOIN assets ON assets.asset_id=trans_request.asset_fk JOIN users requester ON requester.user_id=trans_request.requester_id JOIN users approver ON approver.user_id=trans_request.approver_id JOIN facilities source ON source.facility_id=trans_request.source JOIN facilities destination ON destination.facility_id=trans_request.destination" >transfers.csv
 
 cd ..
