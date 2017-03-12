26d371738fa9be1ee96b525c4bec7e9be556752d
lbchavarria <lbchavarria@gmail.com>
2017-03-12 19:08:01 +0000

    import_data_sh now imports assets.csv, won't insert into asset_at or trans_request

--- import/import_data.sh
+++ import/import_data.sh
@@ -13,7 +13,7 @@ psql -d $1 -c "DROP TABLE IF EXISTS temp"
 psql -d $1 -c "CREATE TABLE temp (username varchar(16) DEFAULT NULL, password varchar(16) DEFAULT NULL, title varchar(20) DEFAULT NULL, active boolean DEFAULT NULL, code varchar(6) DEFAULT NULL, name varchar(32) DEFAULT NULL, asset_tag varchar(16) DEFAULT NULL, description text DEFAULT NULL, asset_facility_name varchar(32) DEFAULT NULL, acquired timestamp NULL, disposed timestamp NULL DEFAULT NULL, trans_asset_tag varchar(16) DEFAULT NULL, requester_username varchar(16) DEFAULT NULL, request_time timestamp NULL, approver_username varchar(16) DEFAULT NULL, approval_time timestamp NULL, source_name varchar(32) DEFAULT NULL, destination_name varchar(32) DEFAULT NULL, load timestamp NULL, unload timestamp NULL)"
 psql -d $1 -c "COPY temp (username, password, title, active) FROM '$VAR/users.csv' DELIMITER ',' CSV HEADER"
 psql -d $1 -c "COPY temp (code, name) FROM '$VAR/facilities.csv' DELIMITER ',' CSV HEADER"
-psql -d $1 -c "COPY temp (asset_tag, description, asset_facility_name, acquired, disposed) FROM '$VAR/assets.csv' DELIMITER ',' CSV HEADER"
+psql -d $1 -c "COPY temp (asset_tag, description, asset_facility_name, acquired, disposed) FROM '$VAR/assets.csv' WITH DELIMITER ',' CSV HEADER NULL AS 'NULL'"
 psql -d $1 -c "COPY temp (trans_asset_tag, requester_username, request_time, approver_username, approval_time, source_name, destination_name, load, unload) FROM '$VAR/transfers.csv' DELIMITER ',' CSV HEADER"
 
 psql -d $1 -c "INSERT INTO users (username, password, role_fk, active) SELECT username, password, 1, active FROM temp WHERE title='Logistics Officer' AND username IS NOT NULL"
@@ -23,9 +23,5 @@ psql -d $1 -c "INSERT INTO assets (asset_tag, description) SELECT asset_tag, des
 psql -d $1 -c "INSERT INTO asset_at (asset_fk, facility_fk, acquired, disposed) SELECT asset_id, facility_id, acquired, disposed FROM assets JOIN temp ON assets.asset_tag=temp.asset_tag JOIN facilities ON facilities.name=temp.asset_facility_name WHERE temp.asset_tag IS NOT NULL"
 psql -d $1 -c "INSERT INTO trans_request (requester_id, request_time, source, destination, asset_fk, approver_id, approval_time, load, unload) SELECT requester.user_id, request_time, source.facility_id, destination.facility_id, asset_id, approver.user_id, approval_time, load, unload FROM temp JOIN users requester ON requester.username=temp.requester_username JOIN facilities source ON source.name=temp.source_name JOIN facilities destination ON destination.name=temp.destination_name JOIN assets ON assets.asset_tag=temp.trans_asset_tag JOIN users approver ON approver.username=temp.approver_username WHERE trans_asset_tag IS NOT NULL"
 
-#psql -d $1 -c "COPY (SELECT username, password, title FROM users JOIN roles ON users.role_fk=roles.role_id) FROM 'users.csv' DELIMITER ',' CSV HEADER"
-#psql -d $1 -c "COPY (SELECT code, name FROM facilities) FROM 'facilities.csv' DELIMITER ',' CSV HEADER"
-#psql -d $1 -c "COPY (SELECT asset_tag, description, name, disposed FROM assets JOIN asset_at ON assets.asset_id=asset_at.asset_fk JOIN facilities ON facilities.facility_id=asset_at.facility_fk) FROM 'assets.csv' DELIMITER ',' CSV HEADER"
-#psql -d $1 -c "COPY (SELECT asset_tag, requester.username, request_time, approver.username, approval_time, source.name, destination.name, load, unload FROM trans_request JOIN assets ON assets.asset_id=trans_request.asset_fk JOIN users requester ON requester.user_id=trans_request.requester_id JOIN users approver ON approver.user_id=trans_request.approver_id JOIN facilities source ON source.facility_id=trans_request.source JOIN facilities destination ON destination.facility_id=trans_request.destination) FROM 'transfers.csv' DELIMITER ',' CSV HEADER"
 
 cd ../../import
